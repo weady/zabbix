90c907ed53f1cac65415497a5a22961a35bcb8df
weady <708964732@qq.com>
2016-03-25 17:27:13 +0800

    update

--- zabbix-2.4.6/scripts/check.baseinfo.sh
+++ zabbix-2.4.6/scripts/check.baseinfo.sh
@@ -11,6 +11,10 @@ case $name in
 		count=`echo "$pro_name" |wc -l`
 		echo $count
 		;;
+	disk_name)
+		disk_name=`cat /proc/diskstats | awk '$3~/sd.*[a-z]$/ {print $3}' | sort`
+		echo $disk_name
+		;;
 	cpu_type)
 		core_num=`cat /proc/cpuinfo | grep "name" | wc -l`
 		type=`cat /proc/cpuinfo | grep "name" | awk '{print $4,$5,$7,$NF }' | head -n 1`
@@ -23,6 +27,10 @@ case $name in
 	mem_top5)
 		name=`top -b n 1 | tail -n +8 | awk '{s[$NF]+=$10} END{for(key in s) print s[key],key}' | sort -nrk 1 | head -n 5 | awk -F '.' 'BEGIN{ORS="|"}{print $1"."$2}'`
                 echo $name
+		;;
+	ip_list)
+		net_ip=`ifconfig |grep  -e 'Link encap' -A1 | grep -v '\-\-' | sed 'N;s/\n//' | awk -F '[[:space:]]*' '{print $1,$7}' | awk -F '[: ]' '{print $1,$NF}' | grep -v 'lo' | awk 'BEGIN{ORS="|"} {print}'`
+		echo $net_ip
 		;;
 	*)
 		echo "Error"
