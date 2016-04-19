f2ac2ec43024cddb2ef414552c6e2b632fac277b
weady <708964732@qq.com>
2016-04-19 13:51:06 +0800

    update

--- zabbix-2.4.6/scripts/check.concurrent.sh
+++ zabbix-2.4.6/scripts/check.concurrent.sh
@@ -6,14 +6,12 @@
 
 
 ilogslaveid=`cat /homed/start.sh | grep "^_restart.*ilogslave.exe" | awk '{print $(NF-1)}' | tr -d "'"`
-type="StbNowMovieCount PadNowMovieCount MobileNowMovieCount PadNowTSCount MobileNowKTSCount NowTRCount StbNowTSCount PcNowDCount PcNowLiveCount NowKTSCount StbNowTotalCount PadNowTotalCount MobileNowTotalCount PcNowKTSCount PcNowMovieCount NowTSCount MobileNowTRCount SmartCardNowKTSCount SmartCardNowDCount MobileNowLiveCount PcNowTotalCount StbNowDCount SmartCardNowTRCount PadNowLiveCount SmartCardNowMovieCount NowMovieCount PcNowTRCount PadNowKTSCount SmartCardNowTotalCount NowTotalCount NowLiveCount MobileNowTSCount PadNowTRCount StbNowTRCount StbNowKTSCount SmartCardNowTSCount NowDCount StbNowLiveCount PadNowDCount PcNowTSCount MobileNowDCount SmartCardNowLiveCount"
-name=`echo "$type" | sed 's/ /\n/g'`
-COUNT=`echo "$name" |wc -l`
+COUNT=`echo "$ilogslaveid" |wc -l`
 INDEX=0
 echo {'"data"':[
-	echo "$name" | while read LINE; 
+	echo "$ilogslaveid" | while read LINE; 
 		do
-    			echo -n '{"{#DEVICETYPE}":"'$ilogslaveid:$LINE'"}'
+    			echo -n '{"{#SRVID}":"'$LINE'"}'
     			INDEX=`expr $INDEX + 1`
     			if [ $INDEX -lt $COUNT ]; then
         			echo ","
