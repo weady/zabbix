2c75572cec6ce236f0321a936d9f57b9d77b5b28
weady <708964732@qq.com>
2016-08-11 13:45:13 +0800

    update

--- zabbix-2.4.6/scripts/auto.partition.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#
-user="zabbix"
-pass="zabbixpass"
-db="zabbix"
-host="127.0.0.1"
-mysql -B -u$user -h$host -p$pass zabbix -e "CALL create_zabbix_partitions();"
-
--- zabbix-2.4.6/scripts/net.monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#
-#
-
-ip -s link | grep 'state' | awk '{print $2,$9}' | grep "$1" | awk -F':' '{print $2}' | sed 's/[[:space:]]//'
